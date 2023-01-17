224642b131f91d25e077a591976e4f01644cafe4
Ryan Bush <cold@coldlogical.com>
2023-01-17 17:38:02 +0900

    FIX: Updated install to be a zshell script CHANGE: Set spacing to 4 spaces NEW: Added no parameter none returning mock functions

--- MockClass.xctemplate/Swift/___VARIABLE_productName___Mock.swift
+++ MockClass.xctemplate/Swift/___VARIABLE_productName___Mock.swift
@@ -3,18 +3,22 @@ import Foundation
 @testable import ___PROJECTNAME___
 
 class ___VARIABLE_productName___Mock: ___VARIABLE_productName___ {
-        // MARK: - Test Variables
-        var functionsCalled = [String]()
+    // MARK: - Test Variables
+    var functionsCalled = [String]()
     
-        var ___VARIABLE_swiftFunction___Result: <#Function Return Type#>
+    var ___VARIABLE_swiftFunction___Result: <#Function Return Type#>
     
-        // MARK: - Input Variables
-        var ___VARIABLE_swiftFunction___Input: <#Parameter Type#>?
-        
-        // MARK: - Overriden Functions
-        override func ___VARIABLE_swiftFunction___(<#Paramater Name#>: <#Paramater Type#>) -> <#Function Return Type#> {
-                functionsCalled.append(#function)
-                ___VARIABLE_swiftFunction___Input = <#Parameter Name#>
-                return ___VARIABLE_swiftFunction___Result
-        }
+    // MARK: - Input Variables
+    var ___VARIABLE_swiftFunction___Input: <#Parameter Type#>?
+    
+    // MARK: - Overriden Functions
+    func ___VARIABLE_swiftFunction___() {
+        functionsCalled.append(#function)
+    }
+    
+    override func ___VARIABLE_swiftFunction___(<#Paramater Name#>: <#Paramater Type#>) -> <#Function Return Type#> {
+        functionsCalled.append(#function)
+        ___VARIABLE_swiftFunction___Input = <#Parameter Name#>
+        return ___VARIABLE_swiftFunction___Result
+    }
 }
--- MockInterface.xctemplate/Swift/___VARIABLE_productName___Mock.swift
+++ MockInterface.xctemplate/Swift/___VARIABLE_productName___Mock.swift
@@ -3,21 +3,25 @@ import Foundation
 @testable import ___PROJECTNAME___
 
 class ___VARIABLE_productName___Mock {
-        // MARK: - Test Variables
-        var functionsCalled = [String]()
+    // MARK: - Test Variables
+    var functionsCalled = [String]()
     
-        var ___VARIABLE_swiftProtocolFunction___Result: <#Function Return Type#>
+    var ___VARIABLE_swiftProtocolFunction___Result: <#Function Return Type#>
     
-        // MARK: - Input Variables
-        var ___VARIABLE_swiftProtocolFunction___Input: <#Parameter Type#>?
-        
-        // MARK: - Overriden Functions
+    // MARK: - Input Variables
+    var ___VARIABLE_swiftProtocolFunction___Input: <#Parameter Type#>?
+    
+    // MARK: - Overriden Functions
 }
 
 extension ___VARIABLE_productName___Mock: ___VARIABLE_productName___ {
-        func ___VARIABLE_swiftProtocolFunction___(<#Paramater Name#>: <#Paramater Type#>) -> <#Function Return Type#> {
-                functionsCalled.append(#function)
-                ___VARIABLE_swiftProtocolFunction___Input = <#Parameter Name#>
-                return ___VARIABLE_swiftProtocolFunction___Result
-        }
+    func ___VARIABLE_swiftProtocolFunction___() {
+        functionsCalled.append(#function)
+    }
+    
+    func ___VARIABLE_swiftProtocolFunction___(<#Paramater Name#>: <#Paramater Type#>) -> <#Function Return Type#> {
+        functionsCalled.append(#function)
+        ___VARIABLE_swiftProtocolFunction___Input = <#Parameter Name#>
+        return ___VARIABLE_swiftProtocolFunction___Result
+    }
 }
